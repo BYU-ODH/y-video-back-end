f134a04a0fcc59bb2f00312a0589f293fc0a05e7
Matthew Cheney <m.cheney95@outlook.com>
2020-05-14 15:32:30 -0600

    reformatted tests

--- test/clj/y_video_postgres_swagger/test/db/core.clj
+++ test/clj/y_video_postgres_swagger/test/db/core.clj
@@ -18,80 +18,43 @@
     (f)))
 
 (deftest test-account
-  (jdbc/with-transaction [t-conn *db* {:rollback-only true}]
-    (let [res (db/add-account!
-              t-conn
-              {:email "test@gmail.com"
-               :lastlogin "never"
-               :name "Buster"
-               :role 0
-               :username "b1"})]
-               (is (= 1 (count res)))
-               (is (= {
-                 :account_id (:account_id (get res 0))
-                 :email "test@gmail.com"
-                 :lastlogin "never"
-                 :name "Buster"
-                 :role 0
-                 :username "b1"
-                 } (db/get-account t-conn {:account_id (:account_id (get res 0))}))))))
+ (jdbc/with-transaction [t-conn *db* {:rollback-only true}]
+   (let [args {:email "me@gmail.com" :lastlogin "sometime" :name "will" :role 0 :username "conquerer01"}]
+   (let [res (db/add-account!
+             t-conn
+             args)]
+              (is (= 1 (count res)))
+              (is (= (into args {:account_id (:account_id (get res 0))}) (db/get-account t-conn {:account_id (:account_id (get res 0))})))))))
 
 (deftest test-tword
  (jdbc/with-transaction [t-conn *db* {:rollback-only true}]
-   (let [account_id nil tword "a word!" src_lang "ru" dest_lang "en"]
+   (let [args {:account_id nil :tword "a word!" :src_lang "ru" :dest_lang "en"}]
    (let [res (db/add-tword!
              t-conn
-             {
-               :account_id account_id
-               :tword tword
-               :src_lang src_lang
-               :dest_lang dest_lang
-               })]
-               (print res)
+             args)]
               (is (= 1 (count res)))
-              (is (= {
-                :tword_id (:tword_id (get res 0))
-                :account_id account_id
-                :tword tword
-                :src_lang src_lang
-                :dest_lang dest_lang
-                } (db/get-tword t-conn {:tword_id (:tword_id (get res 0))})))))))
+              (is (= (into args {:tword_id (:tword_id (get res 0))}) (db/get-tword t-conn {:tword_id (:tword_id (get res 0))})))))))
 
 (deftest test-collection
  (jdbc/with-transaction [t-conn *db* {:rollback-only true}]
-   (let [name "collection name!" published false archived false]
+   (let [args {:name "collection name!" :published false :archived false}]
    (let [res (db/add-collection!
              t-conn
-             {
-               :name name
-               :published published
-               :archived archived
-               })]
+             args)]
               (is (= 1 (count res)))
-              (is (= {
-                :collection_id (:collection_id (get res 0))
-                :name name
-                :published published
-                :archived archived
-                } (db/get-collection t-conn {:collection_id (:collection_id (get res 0))})))))))
+              (is (=
+                (into args {:collection_id (:collection_id (get res 0))}) (db/get-collection t-conn {:collection_id (:collection_id (get res 0))})))))))
 
 (deftest test-course
  (jdbc/with-transaction [t-conn *db* {:rollback-only true}]
-   (let [department "Russian" catalog_number "421" section_number "001"]
+   (let [args {:department "Russian" :catalog_number "421" :section_number "001"}]
    (let [res (db/add-course!
              t-conn
-             {
-               :department department
-               :catalog_number catalog_number
-               :section_number section_number
-               })]
+             args)]
               (is (= 1 (count res)))
-              (is (= {
-                :course_id (:course_id (get res 0))
-                :department department
-                :catalog_number catalog_number
-                :section_number section_number
-                } (db/get-course t-conn {:course_id (:course_id (get res 0))})))))))
+              (is (=
+                (into args {:course_id (:course_id (get res 0))})
+                (db/get-course t-conn {:course_id (:course_id (get res 0))})))))))
 
 (deftest test-content
  (jdbc/with-transaction [t-conn *db* {:rollback-only true}]
