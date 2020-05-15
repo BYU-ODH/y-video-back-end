8657578721b02ea44245accd2224bbd6b2cc9e76
Matthew Cheney <m.cheney95@outlook.com>
2020-05-15 12:56:23 -0600

    added more delete queries

--- resources/sql/deletes.sql
+++ resources/sql/deletes.sql
@@ -1,9 +1,36 @@
 /* DELETE BASE TABLE STATEMENTS */
 
+-- :name delete-account :? :n
+-- :doc deletes account with given account_id
+DELETE FROM Account
+WHERE account_id = :account_id
+
+-- :name delete-tword :? :n
+-- :doc deletes tword with given tword_id
+DELETE FROM TWord
+WHERE tword_id = :tword_id
+
 -- :name delete-collection :? :n
 -- :doc deletes collection with given collection_id
 DELETE FROM Collection
 WHERE collection_id = :collection_id
+
+-- :name delete-course :? :n
+-- :doc deletes course with given course_id
+DELETE FROM Course
+WHERE course_id = :course_id
+
+-- :name delete-content :? :n
+-- :doc deletes content with given content_id
+DELETE FROM Content
+WHERE content_id = :content_id
+
+-- :name delete-file :? :n
+-- :doc deletes file with given file_id
+DELETE FROM File
+WHERE file_id = :file_id
+
+
 
 /* DELTE MANY-TO-MANY TABLE STATEMENTS */
 
