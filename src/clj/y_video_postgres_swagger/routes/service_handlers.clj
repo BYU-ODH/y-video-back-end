f30ef2642489a22a15722d3f7a31b7ce29422f98
Matthew Cheney <m.cheney95@outlook.com>
2020-05-19 09:30:18 -0600

    moved handler logic

---
+++ src/clj/y_video_postgres_swagger/routes/service_handlers.clj
@@ -0,0 +1,181 @@
+(ns y-video-postgres-swagger.routes.service_handlers
+  (:require
+    [y-video-postgres-swagger.dbaccess.access :as db-access]))
+
+
+(def user-create ;; Non-functional
+  {:summary "Creates a new user - FOR DEVELOPMENT ONLY"
+   :parameters {:body {:id string? :name string? :published boolean? :archived boolean?
+                       :assoc_courses [{:id string? :department string? :catalog_number string? :section_number string?}]
+                       :assoc_users [{:id string? :role int?}]
+                       :assoc_content [{:id string? :name string? :thumbnail string? :published boolean?
+                                        :allow_definitions boolean? :allow_notes boolean? :allow_captions string?}]}}
+   :responses {200 {:body {:message string?}}}
+   :handler (fn [{{{:keys [id name published archived]} :body} :parameters}]
+             {:status 200
+              :body (db-access/add_collection id name published archived)})})
+
+(def user-get-by-id ;; Non-functional
+  {:summary "Retrieves the current logged-in user"
+   :parameters {:query {:user_id string?}}
+   :responses {200 {:body {:user_id string? :email string? :lastlogin string? :name string? :role int? :username string?}}
+               404 {:body {:message string?}}}
+   :handler (fn [{{{:keys [user_id]} :query} :parameters}]
+             (let [user_result (db-access/get_user user_id)]
+              (if (nil? user_result)
+                {:status 404
+                 :body {:message "requested user not found"}}
+                {:status 200
+                 :body user_result})))})
+
+(def user-update ;; Non-functional
+  {:summary "Updates specified user"
+   :parameters {}
+   :responses {200 {:body {:message string?}}}
+   :handler (fn [args] {:status 200
+                        :body {:message "placeholder"}})})
+
+(def user-delete ;; Non-functional
+  {:summary "Deletes specified user"
+   :parameters {}
+   :responses {200 {:body {:message string?}}}
+   :handler (fn [args] {:status 200
+                        :body {:message "placeholder"}})})
+
+(def collection-get-all-by-user ;; Non-functional
+  {:summary "Retrieves collection info for all collections available to the current user_id"
+   :parameters {:query {:user_id string?}}
+   :responses {200 {:body [{:collection_id string? :name string? :published boolean? :archived boolean?}]}
+               404 {:body {:message string?}}}
+   :handler (fn [{{{:keys [user_id]} :query} :parameters}]
+             (let [collection_result (db-access/get_collections user_id)]
+              (if (nil? collection_result)
+                {:status 404
+                 :body {:message "requested collection not found"}}
+                {:status 200
+                 :body collection_result})))})
+
+(def collection-create ;; Non-functional
+  {:summary "Creates a new collection with the current user as an owner"
+   :parameters {:body {:current_user_id string? :name string? :published boolean? :archived boolean?}}
+   :responses {200 {:body {:message string?}}}
+   :handler (fn [{{{:keys [current_user_id name published archived]} :body} :parameters}]
+             {:status 200
+              :body (db-access/add_collection current_user_id name published archived)})})
+
+(def collection-get-by-id ;; Non-functional
+  {:summary "Retrieves the specified collection"
+   :responses {200 {:body {:collection_id string? :name string? :published boolean? :archived boolean?}}
+               404 {:body {:message string?}}}
+   :handler (fn [{{{:keys [collection_id]} :query} :parameters}]
+             (let [collection_result (db-access/get_collection collection_id)]
+              (if (nil? collection_result)
+                {:status 404
+                 :body {:message "requested collection not found"}}
+                {:status 200
+                 :body collection_result})))})
+
+(def collection-update ;; Non-functional
+  {:summary "Updates the specified collection"
+   :parameters {:body {:name string? :published boolean? :archived boolean?}}
+   :responses {200 {:body {:message string?}}}
+   :handler (fn [args] {:status 200
+                        :body {:message "placeholder"}})})
+
+(def collection-delete ;; Non-functional
+  {:summary "Deletes the specified collection"
+   :parameters {}
+   :responses {200 {:body {:message string?}}}
+   :handler (fn [args] {:status 200
+                        :body {:message "placeholder"}})})
+
+(def collection-get-all-contents ;; Non-functional
+  {:summary "Retrieves all the content for the specified collection"
+   :parameters {:query {:collection_id string?}}
+   :responses {200 {:body {:collection_id string? :name string? :published boolean? :archived boolean?}}
+               404 {:body {:message string?}}}
+   :handler (fn [{{{:keys [collection_id]} :query} :parameters}]
+             (let [collection_result (db-access/get_collections collection_id)]
+              (if (nil? collection_result)
+                {:status 404
+                 :body {:message "requested collection not found"}}
+                {:status 200
+                 :body collection_result})))})
+
+(def course-create ;; Non-functional
+  {:summary "Creates new course"
+   :parameters {}
+   :responses {200 {:body {:message string?}}}
+   :handler (fn [args] {:status 200
+                        :body {:message "placeholder"}})})
+
+(def course-get-by-id ;; Non-functional
+  {:summary "Retrieves the specified course"
+   :parameters {:query {:collection_id string?}}
+   :responses {200 {:body {:collection_id string? :name string? :published boolean? :archived boolean?}}
+               404 {:body {:message string?}}}
+   :handler (fn [{{{:keys [collection_id]} :query} :parameters}]
+             (let [collection_result (db-access/get_collections collection_id)]
+              (if (nil? collection_result)
+                {:status 404
+                 :body {:message "requested collection not found"}}
+                {:status 200
+                 :body collection_result})))})
+
+(def course-update ;; Non-functional
+  {:summary "Updates the specified course"
+   :parameters {:body {:id string? :name string? :published boolean? :archived boolean?
+                       :assoc_courses [{:id string? :department string? :catalog_number string? :section_number string?}]
+                       :assoc_users [{:id string? :role int?}]
+                       :assoc_content [{:id string? :name string? :thumbnail string? :published boolean?
+                                        :allow_definitions boolean? :allow_notes boolean? :allow_captions string?}]}}
+   :responses {200 {:body {:message string?}}}
+   :handler (fn [{{{:keys [id name published archived]} :body} :parameters}]
+             {:status 200
+              :body (db-access/add_collection id name published archived)})})
+
+(def course-delete ;; Non-functional
+  {:summary "Deletes the specified course"
+   :parameters {}
+   :responses {200 {:body {:message string?}}}
+   :handler (fn [args] {:status 200
+                        :body {:message "placeholder"}})})
+
+(def content-create ;; Non-functional
+  {:summary "Creates new content"
+   :parameters {}
+   :responses {200 {:body {:message string?}}}
+   :handler (fn [args] {:status 200
+                        :body {:message "placeholder"}})})
+
+(def content-get-by-id ;; Non-functional
+  {:summary "Retrieves the specified content"
+   :parameters {:query {:collection_id string?}}
+   :responses {200 {:body {:collection_id string? :name string? :published boolean? :archived boolean?}}
+               404 {:body {:message string?}}}
+   :handler (fn [{{{:keys [collection_id]} :query} :parameters}]
+             (let [collection_result (db-access/get_collections collection_id)]
+              (if (nil? collection_result)
+                {:status 404
+                 :body {:message "requested collection not found"}}
+                {:status 200
+                 :body collection_result})))})
+
+(def content-update ;; Non-functional
+  {:summary "Updates the specified content"
+   :parameters {:body {:id string? :name string? :published boolean? :archived boolean?
+                       :assoc_courses [{:id string? :department string? :catalog_number string? :section_number string?}]
+                       :assoc_users [{:id string? :role int?}]
+                       :assoc_content [{:id string? :name string? :thumbnail string? :published boolean?
+                                        :allow_definitions boolean? :allow_notes boolean? :allow_captions string?}]}}
+   :responses {200 {:body {:message string?}}}
+   :handler (fn [{{{:keys [id name published archived]} :body} :parameters}]
+             {:status 200
+              :body (db-access/add_collection id name published archived)})})
+
+(def content-delete ;; Non-functional
+  {:summary "Deletes the specified content"
+   :parameters {}
+   :responses {200 {:body {:message string?}}}
+   :handler (fn [args] {:status 200
+                        :body {:message "placeholder"}})})
--- src/clj/y_video_postgres_swagger/routes/services.clj
+++ src/clj/y_video_postgres_swagger/routes/services.clj
@@ -10,9 +10,9 @@
     [y-video-postgres-swagger.middleware.formats :as formats]
     [y-video-postgres-swagger.middleware.exception :as exception]
     [y-video-postgres-swagger.dbaccess.access :as db-access]
+    [y-video-postgres-swagger.routes.service_handlers :as service-handlers]
     [ring.util.http-response :refer :all]
     [clojure.java.io :as io]))
-
 
 
 (defn service-routes []
@@ -71,180 +71,52 @@
                         {:status 200
                          :body {:echo echo}})}}]]
 
+   ["/user"
+    {:swagger {:tags ["user"]}}
+    [""
+     {:post service-handlers/user-create}]
+    ["/{id}"
+       {:get service-handlers/user-get-by-id
+
+        :patch service-handlers/user-update
+        :delete service-handlers/user-delete}]]
 
    ["/collections"
     {:swagger {:tags ["collections"]}}
 
     [""
-      {:get {:summary "Retrieves collection info for all collections available to the current user_id"
-             :parameters {:query {:user_id string?}}
-             :responses {200 {:body [{:collection_id string? :name string? :published boolean? :archived boolean?}]}
-                         404 {:body {:message string?}}}
-             :handler (fn [{{{:keys [user_id]} :query} :parameters}]
-                       (let [collection_result (db-access/get_collections user_id)]
-                        (if (nil? collection_result)
-                          {:status 404
-                           :body {:message "requested collection not found"}}
-                          {:status 200
-                           :body collection_result})))}}]]
+      {:get service-handlers/collection-get-all-by-user}]]
 
    ["/collection"
     {:swagger {:tags ["collection"]}}
     [""
-     {:post {:summary "Adds a new collection with the current user as an owner"
-             :parameters {:body {:current_user_id string? :name string? :published boolean? :archived boolean?}}
-             :responses {200 {:body {:message string?}}}
-             :handler (fn [{{{:keys [current_user_id name published archived]} :body} :parameters}]
-                       {:status 200
-                        :body (db-access/add_collection current_user_id name published archived)})}}]
+     {:post service-handlers/collection-create}]
     ["/{id}"
-     {:get {:summary "Retrieves the specified collection"
-            :responses {200 {:body {:collection_id string? :name string? :published boolean? :archived boolean?}}
-                        404 {:body {:message string?}}}
-            :handler (fn [{{{:keys [collection_id]} :query} :parameters}]
-                      (let [collection_result (db-access/get_collection collection_id)]
-                       (if (nil? collection_result)
-                         {:status 404
-                          :body {:message "requested collection not found"}}
-                         {:status 200
-                          :body collection_result})))}
+     {:get service-handlers/collection-get-by-id
 
 
-      :patch {:summary "Edits the specified collection"
-              :parameters {:body {:name string? :published boolean? :archived boolean?}}
-              :responses {200 {:body {:message string?}}}
-              :handler (fn [args] {:status 200
-                                   :body {:message "placeholder"}})}}]
+      :patch service-handlers/collection-update
+      :delete service-handlers/collection-delete}]
 
     ["/{id}/content"
-     {:get {:summary "Retrieves all the content for the specified collection"
-            :parameters {:query {:collection_id string?}}
-            :responses {200 {:body {:collection_id string? :name string? :published boolean? :archived boolean?}}
-                        404 {:body {:message string?}}}
-            :handler (fn [{{{:keys [collection_id]} :query} :parameters}]
-                      (let [collection_result (db-access/get_collections collection_id)]
-                       (if (nil? collection_result)
-                         {:status 404
-                          :body {:message "requested collection not found"}}
-                         {:status 200
-                          :body collection_result})))}
+     {:get service-handlers/collection-get-all-contents}]]
+   [ "/course"
+    {:swagger {:tags ["course"]}}
+    [""
+     {:post service-handlers/course-create}]
+    ["/{id}"
+     {:get service-handlers/course-get-by-id
 
-      :post {:summary "Add a new content to the specified collection"
-             :parameters {:body {:id string? :name string? :published boolean? :archived boolean?
-                                 :assoc_courses [{:id string? :department string? :catalog_number string? :section_number string?}]
-                                 :assoc_users [{:id string? :role int?}]
-                                 :assoc_content [{:id string? :name string? :thumbnail string? :published boolean?
-                                                  :allow_definitions boolean? :allow_notes boolean? :allow_captions string?}]}}
-             :responses {200 {:body {:message string?}}}
-             :handler (fn [{{{:keys [id name published archived]} :body} :parameters}]
-                       {:status 200
-                        :body (db-access/add_collection id name published archived)})}}]]
+      :patch service-handlers/course-update
+      :delete service-handlers/course-delete}]]
+
    ["/content"
     {:swagger {:tags ["content"]}}
     [""
-     {:get {:summary "Retrieves the specified content"
-            :parameters {:query {:collection_id string?}}
-            :responses {200 {:body {:collection_id string? :name string? :published boolean? :archived boolean?}}
-                        404 {:body {:message string?}}}
-            :handler (fn [{{{:keys [collection_id]} :query} :parameters}]
-                      (let [collection_result (db-access/get_collections collection_id)]
-                       (if (nil? collection_result)
-                         {:status 404
-                          :body {:message "requested collection not found"}}
-                         {:status 200
-                          :body collection_result})))}
+     {:post service-handlers/content-create}]
 
-      :post {:summary "Edits the specified content"
-             :parameters {:body {:id string? :name string? :published boolean? :archived boolean?
-                                 :assoc_courses [{:id string? :department string? :catalog_number string? :section_number string?}]
-                                 :assoc_users [{:id string? :role int?}]
-                                 :assoc_content [{:id string? :name string? :thumbnail string? :published boolean?
-                                                  :allow_definitions boolean? :allow_notes boolean? :allow_captions string?}]}}
-             :responses {200 {:body {:message string?}}}
-             :handler (fn [{{{:keys [id name published archived]} :body} :parameters}]
-                       {:status 200
-                        :body (db-access/add_collection id name published archived)})}}]]
+    ["/{id}"
+      {:get service-handlers/content-get-by-id
 
-   ["/user"
-    {:swagger {:tags ["user"]}}
-    [""
-     {:get {:summary "Retrieves the current logged-in user"
-            :parameters {:query {:user_id string?}}
-            :responses {200 {:body {:user_id string? :email string? :lastlogin string? :name string? :role int? :username string?}}
-                        404 {:body {:message string?}}}
-            :handler (fn [{{{:keys [user_id]} :query} :parameters}]
-                      (let [user_result (db-access/get_user user_id)]
-                       (if (nil? user_result)
-                         {:status 404
-                          :body {:message "requested user not found"}}
-                         {:status 200
-                          :body user_result})))}
-
-      :post {:summary "TEMPORARY - adds a new user"
-             :parameters {:body {:id string? :name string? :published boolean? :archived boolean?
-                                 :assoc_courses [{:id string? :department string? :catalog_number string? :section_number string?}]
-                                 :assoc_users [{:id string? :role int?}]
-                                 :assoc_content [{:id string? :name string? :thumbnail string? :published boolean?
-                                                  :allow_definitions boolean? :allow_notes boolean? :allow_captions string?}]}}
-             :responses {200 {:body {:message string?}}}
-             :handler (fn [{{{:keys [id name published archived]} :body} :parameters}]
-                       {:status 200
-                        :body (db-access/add_collection id name published archived)})}}]]
-
-
-
-
-
-
-   (comment ["/math"
-             {:swagger {:tags ["math"]}}
-
-             ["/plus"
-              {:get {:summary "plus with spec query parameters"
-                     :parameters {:query {:x int?, :y int?}}
-                     :responses {200 {:body {:total pos-int?}}}
-                     :handler (fn [{{{:keys [x y]} :query} :parameters}]
-                                {:status 200
-                                 :body {:total (+ x y)}})}
-               :post {:summary "plus with spec body parameters"
-                      :parameters {:body {:x int?, :y int?}}
-                      :responses {200 {:body {:total pos-int?}}}
-                      :handler (fn [{{{:keys [x y]} :body} :parameters}]
-                                 {:status 200
-                                  :body {:total (+ x y)}})}}]])
-
-   (comment ["/files"
-             {:swagger {:tags ["files"]}}
-
-             ["/upload"
-              {:post {:summary "upload a file"
-                      :parameters {:multipart {:file multipart/temp-file-part}}
-                      :responses {200 {:body {:name string?, :size int?}}}
-                      :handler (fn [{{{:keys [file]} :multipart} :parameters}]
-                                 {:status 200
-                                  :body {:name (:filename file)
-                                         :size (:size file)}})}}]
-
-             ["/download"
-              {:get {:summary "downloads a file"
-                     :swagger {:produces ["image/png"]}
-                     :handler (fn [_]
-                                {:status 200
-                                 :headers {"Content-Type" "image/png"}
-                                 :body (-> "public/img/warning_clojure.png"
-                                           (io/resource)
-                                           (io/input-stream))})}}]])])
-(comment :post {:summary "Adds a new collection with the current user as an owner"}
-       :parameters {:body {:current_user_id string? :name string? :published boolean? :archived boolean?
-                           :assoc_courses [{:id string? :department string? :catalog_number string? :section_number string?}]
-                           :assoc_users [{:id string? :role int?}]
-                           :assoc_content [{:id string? :name string? :thumbnail string? :published boolean?
-                                            :allow_definitions boolean? :allow_notes boolean? :allow_captions string?}]}}
-                   :responses {200 {:body {:collection_id string? :name string? :published boolean? :archived boolean?
-                                           :assoc_courses [{:course_id string? :department string? :catalog_number string? :section_number string?}]
-                                           :assoc_users [{:user_id string? :role int?}]
-                                           :assoc_content [{:content_id string? :name string? :thumbnail string? :published boolean?
-                                                            :allow_definitions boolean? :allow_notes boolean? :allow_captions string?}]}}}
-                   :handler (fn [{{{:keys [current_user_id name published archived assoc_users assoc_courses assoc_content]} :body} :parameters}]
-                             {:status 200
-                              :body (db-access/add_collection current_user_id name published archived assoc_users assoc_content assoc_courses)}))
+       :patch service-handlers/content-update
+       :delete service-handlers/content-delete}]]])
