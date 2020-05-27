94060c0cb8a4abd76e863e606b4759420a2d2a99
Tory S. Anderson <torys.anderson@gmail.com>
2020-05-27 10:49:46 -0600

    spcing and delete

--- src/clj/y_video_back/db/template.clj
+++ src/clj/y_video_back/db/template.clj
@@ -1,10 +1,10 @@
 (ns y-video-back.db.TABLE
   (:require [y-video-back.db.core :as db]))
 
-   (def CREATE (partial db/CREATE :TABLE))
-   (def READ  (partial db/READ :TABLE_undeleted))
-   (def READ-ALL  (partial db/READ :TABLE))
-   (def UPDATE (partial db/UPDATE :TABLE))
-   (def DELETE (partial db/mark-deleted :TABLE))
-   (def CLONE (partial db/CLONE :TABLE))
-   (def PERMANENT-DELETE (partial db/DELETE :TABLE))
+(def CREATE (partial db/CREATE :TABLE))
+(def READ  (partial db/READ :TABLE_undeleted))
+(def READ-ALL  (partial db/READ :TABLE))
+(def UPDATE (partial db/UPDATE :TABLE))
+(def DELETE (partial db/mark-deleted :TABLE))
+(def CLONE (partial db/CLONE :TABLE))
+(def PERMANENT-DELETE (partial db/DELETE :TABLE))
