44441677b71c7c1ad9b9b1f4632a3b029a1ba503
Matthew Cheney <m.cheney95@outlook.com>
2020-06-01 13:36:49 -0600

    added function to filter db-only fields

--- test/clj/unit/y_video_back/routes/test_user.clj
+++ test/clj/unit/y_video_back/routes/test_user.clj
@@ -37,12 +37,24 @@
   [res]
   (:id (m/decode-response-body res)))
 
+(defn remove-db-only
+  "Compares 2 maps, not counting created, updated, and deleted fields"
+  [my_map]
+  (dissoc my_map :created :updated :deleted))
+
 
 (deftest test-app
-  (testing "user"
-    (let [user_one (g/get_random_user_without_id)]
+  (testing "user CRUD"
+    ; Create user
+    (let [user_one (g/get_random_user_without_id)
+          useR_two (g/get_random_user_without_id)]
      (let [response (rp/user-post user_one)]
+       ; Verify user created
        (is (= 200 (:status response)))
        (let [user_one_id (get-id response)]
+         ; Get user
          (let [response (rp/user-id-get user_one_id)]
-           (is (= 200 (:status response)))))))))
+           ; Verify correct get
+           (is (= 200 (:status response)))
+           (is (= (into user_one {:id user_one_id}) (remove-db-only (m/decode-response-body response))))))))))
+         ; Update user - all fields
