42abd54a3fa424a4bdfe6c1f3a09a5de5e0cff51
Matthew Cheney <m.cheney95@outlook.com>
2020-06-11 15:20:28 -0600

    added default roles

--- src/clj/y_video_back/routes/service_handlers/role_utils.clj
+++ src/clj/y_video_back/routes/service_handlers/role_utils.clj
@@ -43,15 +43,31 @@
     (let [user-id (token-to-user-id token)
           user-type (get-user-type user-id)]
       (case route
-        "user-create" true
+
+        ; Misc handlers
         "echo-post" (<= user-type 2)
-        "collection-create" (<= user-type 2)
-        "collection-update" (or (<= user-type 1)
-                                (<= (get-user-role-coll user-id (:collection-id args)) 1))
-        "collection-delete" (<= user-type 0)
+        "echo-patch" (or (<= user-type 0) false)
+        "connect-collection-and-course" (or (<= user-type 0) false)
+        "search-by-term" (or (<= user-type 0) false)
+
+        ; User handlers
+        "user-create" true ; For development only
+        "user-get-by-id" (or (<= user-type 0) false)
+        "user-update" (or (<= user-type 0) false)
+        "user-delete" (or (<= user-type 0) false)
+        "user-get-logged-in" (or (<= user-type 0) false)
+        "user-get-all-collections" (or (<= user-type 0) false)
+        "user-get-all-courses" (or (<= user-type 0) false)
+        "user-get-all-words" (or (<= user-type 0) false)
+
+        ; Collection handlers
+        "collection-create" (or (<= user-type 2))
         "collection-get-by-id" (or (<= user-type 2)
                                    (<= (get-user-role-coll user-id (:collection-id args)) 2)
                                    (user-crse-coll user-id (:collection-id args)))
+        "collection-update" (or (<= user-type 1)
+                                (<= (get-user-role-coll user-id (:collection-id args)) 1))
+        "collection-delete" (or (<= user-type 0))
         "collection-add-user" (or (<= user-type 1)
                                   (<= (get-user-role-coll user-id (:collection-id args)) 0))
         "collection-remove-user" (or (<= user-type 1)
@@ -60,6 +76,55 @@
                                      (<= (get-user-role-coll user-id (:collection-id args)) 0))
         "collection-remove-content" (or (<= user-type 1)
                                         (<= (get-user-role-coll user-id (:collection-id args)) 0))
+        "collection-add-course" (or (<= user-type 0) false)
+        "collection-remove-course" (or (<= user-type 0) false)
+        "collection-get-all-contents" (or (<= user-type 0) false)
+        "collection-get-all-courses" (or (<= user-type 0) false)
+        "collection-get-all-users" (or (<= user-type 0) false)
+
+        ; Content handlers
+        "content-create" (or (<= user-type 0) false)
+        "content-get-by-id" (or (<= user-type 0) false)
+        "content-update" (or (<= user-type 0) false)
+        "content-delete" (or (<= user-type 0) false)
+        "content-get-all-collections" (or (<= user-type 0) false)
+        "content-get-all-files" (or (<= user-type 0) false)
+        "content-add-view" (or (<= user-type 0) false)
+        "content-add-file" (or (<= user-type 0) false)
+        "content-remove-file" (or (<= user-type 0) false)
+
+        ; File handlers
+        "file-create" (or (<= user-type 0) false)
+        "file-get-by-id" (or (<= user-type 0) false)
+        "file-update" (or (<= user-type 0) false)
+        "file-delete" (or (<= user-type 0) false)
+        "file-get-all-contents" (or (<= user-type 0) false)
+
+        ; Course handlers
+        "course-create" (or (<= user-type 0) false)
+        "course-get-by-id" (or (<= user-type 0) false)
+        "course-update" (or (<= user-type 0) false)
+        "course-delete" (or (<= user-type 0) false)
+        "course-add-collection" (or (<= user-type 0) false)
+        "course-remove-collection" (or (<= user-type 0) false)
+        "course-get-all-collections" (or (<= user-type 0) false)
+        "course-add-user" (or (<= user-type 0) false)
+        "course-remove-user" (or (<= user-type 0) false)
+        "course-get-all-users" (or (<= user-type 0) false)
+
+        ; Word handlers
+        "word-create" (or (<= user-type 0) false)
+        "word-get-by-id" (or (<= user-type 0) false)
+        "word-update" (or (<= user-type 0) false)
+        "word-delete" (or (<= user-type 0) false)
+
+        ; Annotation handlers
+        "annotation-create" (or (<= user-type 0) false)
+        "annotation-get-by-id" (or (<= user-type 0) false)
+        "annotation-update" (or (<= user-type 0) false)
+        "annotation-delete" (or (<= user-type 0) false)
+
+
         false))))
 
 (def forbidden-page
