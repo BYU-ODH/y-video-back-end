b281dd2ccbb713bd00037f3f2ad383dd684a988e
root <root@yvideobeta.byu.edu>
2020-09-30 14:33:41 -0600

    server-side updates

--- resources/public/service-worker.js
+++ resources/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0976ded0681494f80a003161f25b4865.js"
+  "/precache-manifest.21ecaeae276211c7c4356e4a82e3f17f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
