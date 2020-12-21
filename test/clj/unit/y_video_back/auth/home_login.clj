09af3242de748bcbc44bee294dac1fca4eb71004
Matthew Cheney <m.cheney95@outlook.com>
2020-12-21 09:48:48 -0700

    Remove api/ping test

--- test/clj/unit/y_video_back/auth/home_login.clj
+++ test/clj/unit/y_video_back/auth/home_login.clj
@@ -36,11 +36,11 @@
       (is (= 302 (:status res)))
       (is (= (str "https://cas.byu.edu/cas/login?service=" (:host env) "/")
              (get-in res [:headers "Location"])))))
-  (testing "no login, access api/ping"
-    (let [res (app (request :get "/api/ping"))]
-      (is (= 200 (:status res)))
-      (is (= {:message "pong"}
-             (m/decode-response-body res)))))
+  ; (testing "no login, access api/ping"
+  ;   (let [res (app (request :get "/api/ping"))]
+  ;     (is (= 200 (:status res)))
+  ;     (is (= {:message "pong"}
+  ;            (m/decode-response-body res)))))
   (testing "no login, access admin"
     (let [res (app (request :get "/admin"))]
       (is (= 302 (:status res)))
