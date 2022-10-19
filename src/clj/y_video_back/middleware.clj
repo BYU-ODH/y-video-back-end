fc5c2e51301a47f629435d76370a2c6b132c2106
Tory Anderson <torys.anderson@gmail.com>
2022-10-19 11:24:02 -0600

    fails with new code?

--- src/clj/y_video_back/middleware.clj
+++ src/clj/y_video_back/middleware.clj
@@ -34,6 +34,8 @@
 (defn wrap-cas [handler]
     "Validates CAS login. If invalid, only prompts login if given /login path-info"
   (fn [request]
+    #_((cas/wrap-cas handler {:timeout 120 :host-override (:host env) :no-redirect? (constantly (not (= "/login" (str (:path-info request)))))})
+     request)
     (let [should-redirect? #(not= "/login" (str (:path-info request)))
           cas-map {:timeout 120
                    :host-override (:host env)
