21bacb8be9a99eedb3b043b2bae88b5b87bbb201
Tory Anderson <torys.anderson@gmail.com>
2023-09-02 12:18:13 -0600

    remove spies, refactor long lines, update query logic for readability

--- src/clj/y_video_back/db/core.clj
+++ src/clj/y_video_back/db/core.clj
@@ -174,8 +174,6 @@
 
 ; - - - - - - - more generic functions - - - - - - - ;
 
-(def spy #(do (println "DEBUG:" %) %))
-
 (defn update-resource-access-last-verified  ; TODO - generalize this function
   "Sets last verified in resource-access to current timestamp"
   [id]
@@ -185,18 +183,17 @@
 (defn read-where-and
   "Get entry from table by column(s), conditionals joined by AND"
   [table-keyword [& column-keywords] [& column-vals] &[select-field-keys]]
-  (if (= (count column-keywords) (count column-vals))
-    (cond-> {:select (or select-field-keys [:*])
-             :from [table-keyword]}
-      (> (count column-keywords) 0) (assoc :where (into [:and] (map #(vector := %1 %2) column-keywords column-vals))) ;; could this be done with zipmap?
-      true sql/format
-      #_#_true (spy) ; <-- prints sql code just before it's executed
-      true dbr)
-    
-    (throw (ex-info "wrong arg syntax. Args need to be colls" {:cause :checking-for-arg-length-match
-                                                            :column-keywords column-keywords
-                                                            :column-vals column-vals}))
-    ))
+  (let [run-the-query #(cond-> {:select (or select-field-keys [:*])
+                                :from [table-keyword]}
+                         (> (count column-keywords) 0) (assoc :where (into [:and] (map #(vector := %1 %2) column-keywords column-vals))) ;; could this be done with zipmap?
+                         true sql/format
+                         true dbr) ]
+    (if (= (count column-keywords) (count column-vals))
+      (run-the-query)
+      (throw (ex-info "wrong arg syntax. Args need to be colls of equal length"
+                      {:cause :checking-for-arg-length-match
+                       :column-keywords column-keywords
+                       :column-vals column-vals})))))
 
 (defn read-where-or
   "Get entry from table by column(s), conditionals joined by OR"
@@ -206,12 +203,11 @@
              :from [table-keyword]}
       (> (count column-keywords) 0) (assoc :where (into [:or] (map #(vector := %1 %2) column-keywords column-vals)))
       true sql/format
-      ;;true (spy)
       true dbr)
-    (throw (ex-info "wrong arg syntax. Args need to be colls" {:cause :checking-for-arg-length-match
-                                                               :column-keywords column-keywords
-                                                               :column-vals column-vals}))))
-
+    (throw (ex-info "wrong arg syntax. Args need to be colls"
+                    {:cause :checking-for-arg-length-match
+                     :column-keywords column-keywords
+                     :column-vals column-vals}))))
 
 (defn delete-where-and
   "Update delete status in table by column-keywords"
@@ -225,7 +221,6 @@
                       (assoc :deleted (t/now)))]
       (UPDATE table-keyword id val-map))))
 
-
 (defn read-all-where
   "Get all entries from table by column"
   [table-keyword column-keyword &[id select-field-keys]]
@@ -233,10 +228,8 @@
            :from [table-keyword]}
     id (assoc :where [:= column-keyword id])
     true sql/format
-    ;true (spy)
     true dbr))
-    ;(= 1 (count select-field-keys)) (#((first select-field-keys) %))))
-
+    
 (defn read-all-pattern
   "Get all entries from table by column and pattern"
   [table-keyword column-keywords pattern &[select-field-keys]]
@@ -244,7 +237,6 @@
           true (helpers/from table-keyword)
           (> (count column-keywords) 0) (helpers/where [:!= :id ut/nil-uuid] (into [:or] (map #(vector :ilike %1 pattern) column-keywords)))
           true sql/format
-          ;true (spy)
           true dbr))
 
 (defn increment-field
@@ -254,7 +246,6 @@
           true (helpers/sset {column-keyword (sql/call :+ column-keyword 1)})
           true (helpers/where := :id row-id)
           true sql/format
-          ;true (spy)
           true dbdo!))
 
 (defn read-all
@@ -263,10 +254,9 @@
   (cond-> {:select (or select-field-keys [:*])
            :from [table-keyword]}
     true sql/format
-    ;true (spy)
     true dbr))
 
-; - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ;
+; - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
 (defn DELETE
   "Generic delete"
