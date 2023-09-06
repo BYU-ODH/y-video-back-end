cc6167b105cbd7fa38845861356e6a06b2d6ce6f
Tory Anderson <torys.anderson@gmail.com>
2023-09-06 16:41:54 -0600

    DO NOT MERGE turned on five three reads

--- test/clj/usecase/legacy/routes/collection.clj
+++ test/clj/usecase/legacy/routes/collection.clj
@@ -178,11 +178,11 @@
       (is (= (:username no-db-user-one) (:username (first (users/READ-BY-USERNAME [(:username no-db-user-one)])))) "User was not created by db-pop")
       (is (= (:username no-db-user-thr) (:username (first (users/READ-BY-USERNAME [(:username no-db-user-thr)])))) "User was not created by db-pop")
       (let [no-db-user-one-res (first (users/READ-BY-USERNAME [(:username no-db-user-one)]))
-            #_#_no-db-user-thr-res (first (users/READ-BY-USERNAME [(:username no-db-user-thr)]))
-            #_#_res-one (rp/collections-by-logged-in (uc/user-id-to-session-id (:id no-db-user-one-res)))
-            #_#_res-two (rp/collections-by-logged-in (uc/user-id-to-session-id (:id user-two)))
-            #_#_res-thr (rp/collections-by-logged-in (uc/user-id-to-session-id (:id no-db-user-thr-res)))
-            #_#_res-fou (rp/collections-by-logged-in (uc/user-id-to-session-id (:id user-fou)))]
+            no-db-user-thr-res (first (users/READ-BY-USERNAME [(:username no-db-user-thr)]))
+            res-one (rp/collections-by-logged-in (uc/user-id-to-session-id (:id no-db-user-one-res)))
+            res-two (rp/collections-by-logged-in (uc/user-id-to-session-id (:id user-two)))
+            res-thr (rp/collections-by-logged-in (uc/user-id-to-session-id (:id no-db-user-thr-res)))
+            res-fou (rp/collections-by-logged-in (uc/user-id-to-session-id (:id user-fou)))]
         #_(is (= [{:username (:username user-fou)
                  :collection-id (:id coll-one)
                  :account-role 0}]
