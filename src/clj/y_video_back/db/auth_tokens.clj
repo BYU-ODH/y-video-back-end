043281e91c4f36af22dd53801374d37808c7a4b6
Tory Anderson <torys.anderson@gmail.com>
2023-09-12 14:45:48 -0600

    DO NOT MERGE local tests good, logging added, seeing why it is failing

--- src/clj/y_video_back/db/auth_tokens.clj
+++ src/clj/y_video_back/db/auth_tokens.clj
@@ -1,8 +1,7 @@
 (ns y-video-back.db.auth-tokens
   (:require [y-video-back.config :refer [env]]
             [y-video-back.db.core :as db]
-            [taoensso.timbre :as log]
-            [tupelo.core :as t]))
+            [taoensso.timbre :as log]))
 
 (def CREATE (partial db/CREATE :auth-tokens))
 (def READ  (partial db/READ :auth-tokens-undeleted))
@@ -12,48 +11,27 @@
 (def CLONE (partial db/CLONE :auth-tokens))
 (def PERMANENT-DELETE (partial db/DELETE :auth-tokens))
 
-(defn READ-UNEXPIRED ;; original being refactored
-  "Reads unexpired auth-tokens. If expired, deletes and returns nil."
-  [auth-token-id]
-  (log/debug "" {:auth-token-id (str auth-token-id)})
-  (if (nil? auth-token-id)
-    nil
-    (let [auth-token (READ auth-token-id)]
-      (log/debug "" {:auth-token-id (str auth-token-id)
-                     :auth-token (str auth-token)})
-      (when auth-token        
-        (if-not (< (inst-ms (:created auth-token)) (- (System/currentTimeMillis) (-> env :auth :timeout)))
-          auth-token
-          (do (DELETE auth-token-id)
-              nil))))))
-
-#_(defn READ-UNEXPIRED ;; new one
-  "Reads unexpired auth-tokens. If expired, deletes and returns nil."
-  [auth-token-id]
-  (let [auth-token (READ auth-token-id)
-        created (-> auth-token :created)
-        ms-created (when created (inst-ms created))]
-     (if-not (< ms-created (- (System/currentTimeMillis) (-> env :auth :timeout)))
-       auth-token
-       (do (DELETE auth-token-id)
-           nil)))) ;; TODO resume refactoring carefully here
-(comment
-  (when-let [a (:a {:b "no a"})]
-    "It was when") ;; => nil
-  )
-#_(defn READ-UNEXPIRED
+(defn READ-UNEXPIRED
   "Reads unexpired auth-tokens. If expired, deletes and returns nil."
   [auth-token-id]
   (when auth-token-id
     (let [auth-token (READ auth-token-id)
-          now (System/currentTimeMillis)
-          created (when auth-token (inst-ms (:created auth-token)))
           timeout (-> env :auth :timeout)
-          expired? (< created (- now timeout))
-          expired-delete-this! (fn [] (DELETE auth-token-id) nil)]
+          time-created #(inst-ms (:created auth-token))
+          expired? #(< (time-created)
+                       (- (System/currentTimeMillis) timeout))]
       (log/debug "" {:auth-token-id (str auth-token-id)
                      :auth-token (str auth-token)})
-      (if (and auth-token
-               (not expired?))
-        auth-token
-        expired-delete-this!))))
+      (when auth-token
+        (log/debug "Are we expired? If so, why?" {:expired? (expired?)
+                                                  :time-token-created (time-created)
+                                                  :timeout timeout
+                                                  :auth-token auth-token})
+        (if-not (expired?)
+          auth-token
+          (do (DELETE auth-token-id)
+              nil))))))
+
+(comment
+  (log/debug "hi there")
+  )
