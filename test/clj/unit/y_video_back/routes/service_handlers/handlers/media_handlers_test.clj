12f7b7b9415c94994a50ac38924cd730263fcdd3
Tory Anderson <torys.anderson@gmail.com>
2023-10-11 15:25:17 -0600

    WIP streaming tests

--- test/clj/unit/y_video_back/routes/service_handlers/handlers/media_handlers_test.clj
+++ test/clj/unit/y_video_back/routes/service_handlers/handlers/media_handlers_test.clj
@@ -8,22 +8,31 @@
    [legacy.db.test-util :as tcore]
    [legacy.utils.db-populator :as db-pop]
    [legacy.utils.utils :as ut]
-)
-  )
+   [y-video-back.db.files :as files]
+   [muuntaja.core :as m]))
 
 (tcore/basic-transaction-fixtures
  (mount.core/start)
 ; (migratus/renew)
  (ut/renew-db)
  (def user-one (db-pop/add-user "admin"))
- (def res (rp/login-current-user "test")))
+ #_(def res (rp/login-current-user "test")))
 
-(deftest stream-partial-media
+(deftest stream-partial-media-route
   (let [file-key nil
         url nil
         filecontent (ut/get-filecontent)
         file-one (dissoc (db-pop/get-file) :filepath :aspect-ratio)
         res (rp/file-post file-one filecontent)]
-    (testing "Stream partial media refererced by file-key"
-       (is false))
+    (testing "Stream partial media referenced by file-key"
+      (is false))
+    (testing "file READ"
+    (let [file-one (ut/under-to-hyphen (files/CREATE (db-pop/get-file)))
+          res (rp/file-id-get (:id file-one))]
+      (is (= 200 (:status res)))
+      (is (= (-> file-one
+                 (ut/remove-db-only)
+                 (update :id str)
+                 (update :resource-id str))
+             (ut/remove-db-only (m/decode-response-body res))))))
       ))
