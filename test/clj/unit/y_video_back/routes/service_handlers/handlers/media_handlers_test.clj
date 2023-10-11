26b207fd414a0f4cd37b4a149e385755e0ff5f80
Tory Anderson <torys.anderson@gmail.com>
2023-10-11 14:27:53 -0600

    streaming tests

--- test/clj/unit/y_video_back/routes/service_handlers/handlers/media_handlers_test.clj
+++ test/clj/unit/y_video_back/routes/service_handlers/handlers/media_handlers_test.clj
@@ -1,1 +1,29 @@
-(ns y-video-back.routes.service-handlers.handlers.media-handlers-test)
+(ns y-video-back.routes.service-handlers.handlers.media-handlers-test
+  (:require
+   [clojure.test :refer [use-fixtures deftest is testing]]
+   [y-video-back.config :refer [env]]
+   [mount.core :as mount]
+   [legacy.utils.route-proxy.proxy :as rp]
+   [y-video-back.db.migratus :as migratus]
+   [legacy.db.test-util :as tcore]
+   [legacy.utils.db-populator :as db-pop]
+   [legacy.utils.utils :as ut]
+)
+  )
+
+(tcore/basic-transaction-fixtures
+ (mount.core/start)
+; (migratus/renew)
+ (ut/renew-db)
+ (def user-one (db-pop/add-user "admin"))
+ (def res (rp/login-current-user "test")))
+
+(deftest stream-partial-media
+  (let [file-key nil
+        url nil
+        filecontent (ut/get-filecontent)
+        file-one (dissoc (db-pop/get-file) :filepath :aspect-ratio)
+        res (rp/file-post file-one filecontent)]
+    (testing "Stream partial media refererced by file-key"
+       (is false))
+      ))
--- test/clj/unit/y_video_back/routes/service_handlers/handlers_tests.clj
+++ test/clj/unit/y_video_back/routes/service_handlers/handlers_tests.clj
@@ -1,1 +1,16 @@
-(ns y-video-back.routes.service-handlers.handlers-tests)
+(ns y-video-back.routes.service-handlers.handlers-tests
+  (:require
+   [clojure.test :refer [use-fixtures deftest is testing]]
+   [y-video-back.config :refer [env]]
+   [mount.core :as mount]
+   [legacy.utils.route-proxy.proxy :as rp]
+   [y-video-back.db.migratus :as migratus]
+   [legacy.db.test-util :as tcore]
+   [legacy.utils.db-populator :as db-pop]
+))
+
+#_(tcore/basic-transaction-fixtures
+ (mount.core/start)
+ (migratus/renew)
+)
+
