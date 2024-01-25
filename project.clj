3df571c18a4b746f43445115995c8970f7195ea5
Tory Anderson <torys.anderson@gmail.com>
2024-01-25 11:18:11 -0700

    init kawa for ffprobe

--- project.clj
+++ project.clj
@@ -41,7 +41,7 @@
                  [org.clojure/data.json "1.0.0"]
                  [nilenso/honeysql-postgres "0.2.6"]
                  [com.taoensso/timbre "6.0.3"]
-
+                 [w33t/kawa "0.1.2"]
                  [tupelo "23.07.04"]]
 
 
