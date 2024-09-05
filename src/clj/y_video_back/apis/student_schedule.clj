9347f0e1b8a9c208a352908de78b8f00c75d5add
Ben Rencher <rencherb@byu.edu>
2024-09-05 14:40:57 -0600

    adding some code that will eventually replace the old apic alls

--- src/clj/y_video_back/apis/student_schedule.clj
+++ src/clj/y_video_back/apis/student_schedule.clj
@@ -4,14 +4,31 @@
     [clj-http.client :as client]
     [clojure.data.json :as json]))
 
+;; new json parse for courses
+(defn get-courses-from-json-new
+  [json-obj]
+  (get-in json-obj ["data"]) ;; I believe the highest level attribute is "response" and I know that "data" is where the classes are stored in the response
+)
+
 (defn get-courses-from-json
   [js]
   (get-in js ["WeeklySchedService" "response" "schedule_table"]))
 
+;; we shouldn't need this anymore because the data returned by the bdp endpoint is arranged differently - BDR 9/5/2024
 (defn course-to-dep-and-cat
   "Returns vector. 0 is course, 1 is department, 2 is catalog number"
   [course]
   (re-find (re-matcher #"(.*) ([0-9]{3}\w{0,1})" course)))
+
+;; new data extractor to use with new api call - BDR 9/5/2024
+(defn extract-course-data-new
+  [course]
+  {
+    :department (get-in course ["teaching_area"])
+    :catalog-number (get-in course ["catalog_number"])
+    :section-number (get-in course ["section_number"])
+  }
+)
 
 (defn extract-course-data
   [course]
@@ -19,6 +36,18 @@
     {:department (get dep-cat-vector 1)
      :catalog-number (get dep-cat-vector 2)
      :section-number (get-in course ["section"])}))
+
+;; new api for getting student's courses - Ben Rencher 9/5/2024
+(defn get-api-courses-new
+  [netid]
+  (let [url (str "https://api.byu.edu/bdp/student_academics/records/current_student_class_enrollments/v1/?net_id=" netid "&year_term=" (ut/get-current-sem))
+        res (try (client/get url {:oath-token ut/oauth-token})
+                 (catch Exception e
+                    (client/get url {:oath-token (ut/get-oauth-token-new)})))
+        json-res (json/read-str (:body res))]
+    (map extract-course-data-new (get-courses-from-json-new json-res))
+  )
+)
 
 (defn get-api-courses
   [person-id]
--- src/clj/y_video_back/course_creator.clj
+++ src/clj/y_video_back/course_creator.clj
@@ -36,7 +36,9 @@
 
 (defn refresh-courses
   "Queries AcademicRegistrationStudentSchedule api, makes db reflect results"
+  ;; [user-id username] ;; we need username instead of person-id for the new API
   [user-id person-id]
+  ;; let [api-courses (schedule-api/get-api-courses-new username) ;; pass netid (username) in for the new API call
   (let [api-courses (schedule-api/get-api-courses person-id)
         db-courses (get-db-courses user-id)
         to-delete (set (filter #(not (lazy-contains? api-courses (remove-course-db-fields %))) db-courses))
@@ -56,6 +58,7 @@
    (let [user-res (first (users/READ-BY-USERNAME [username]))]
      (if (or force-api (check-last-course-api user-res))
        (do
+         ;; (refresh-coruses (:id user-res) (:username user-res)) ;; this passes the netid instead of byu id. I know that :id and :usernaem are the same, but I don't know if this is always the case for all instances of this function - BDR 9/5/2024
          (refresh-courses (:id user-res) (:byu-person-id user-res))
          (users/UPDATE (:id user-res) {:last-course-api (java.sql.Timestamp. (System/currentTimeMillis))})))))
   ([username]
