d912a12083ffd7ea71b3be3a222d157f22537730
Ben Rencher <rencherb@byu.edu>
2024-09-05 10:51:07 -0600

    adding function to access academic control dates BDP api endpoint

--- src/clj/y_video_back/apis/utils.clj
+++ src/clj/y_video_back/apis/utils.clj
@@ -6,6 +6,33 @@
     [clojure.data.json :as json]
     [clojure.walk :as walk]))
 
+;; new function that uses BDP - Ben Rencher 9/5/2024
+(defn get-current-sem-real-new
+  "Returns the yearterm corresponding to today's date in format YYYYT (Y=year, T=term)"
+  []
+  (let [time-map (vec (t/as (t/zoned-date-time) :year :month-of-year :day-of-month))
+        year (str (get time-map 0))
+        raw-month (int (get time-map 1))
+        raw-day (int (get time-map 2))
+        month (if (< raw-month 10)
+                (str "0" raw-month)
+                (str raw-month))
+        day (if (< raw-day 10)
+              (str "0" raw-day)
+              (str raw-day))
+        date-string (str year month day)]
+    (-> (str "https://api.byu.edu/bdp/student_academics/academic_control_dates/v1/?start_date_time=" date-string)
+        (client/get) ;; makes the call?
+        (:body) ;; gets the body? more like assigns the entire response to the value "body"
+        (json/read-str) ;; parses the json?
+        (walk/keywordize-keys) ;; parses the keys?
+        (:data) ;; name of attribute we want
+        (first) ;; get first value in list from :data
+        (:year_term) ;; get yearterm attribute inside first value of :data
+    )
+  )
+)
+
 (defn get-current-sem-real
   "Returns current semester in format YYYYT (Y=year, T=term)"
   []
