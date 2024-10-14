135374dba8ffb2bb0a20e26a73f02bef65d1d09c
Ben Rencher <rencherb@byu.edu>
2024-10-14 14:30:20 -0600

    adding new functions for switch over to new apis

--- src/clj/y_video_back/apis/persons.clj
+++ src/clj/y_video_back/apis/persons.clj
@@ -5,7 +5,8 @@
     [y-video-back.apis.utils :as ut]
     [clj-http.client :as client]
     [clojure.data.json :as json]
-    [clojure.string :as str]))
+    [clojure.string :as str]
+    [clojure.walk :as walk]))
 
 (defn get-cats-from-json
   [js]
@@ -72,3 +73,119 @@
          :email (str netid "@yvideobeta.byu.edu")
          :account-type (if (:test env) 3 4)
          :person-id "000000000"}))))
+
+(defn get-worker-id
+  "Attempts to get a worker-id, if there is none, the user is not an employee"
+  [byuid]
+  (def url (str "https://api.byu.edu/bdp/iam/byuid_workerid/v1/?byu_id=" byuid))
+  (def response (client/get url {:headers {"Authorization" (ut/get-oauth-token-new)}}))
+  (def json-res (json/read-str (:body response)))
+  (def data (get-in json-res ["data"]))
+  (get-in data ["worker-id"])
+)
+
+(defn is-worker-id-empty
+  "Determines if the worker id is empty"
+  [workerid]
+  (empty? workerid)
+)
+
+(defn get-employee-type
+  "determines if the employee is faculty or something else"
+  [positions]
+  (loop [entries positions]
+   (let [entry (first entries)]
+    (if (entry :is_active_position)
+      {
+        :worker_type_id (entry :employee_or_contingent_worker_type_reference_id)
+        :worker_type (entry :employee_or_contingent_worker_type)
+      } ;; istrue
+      (recur (rest entries)) ;; is false
+    )
+   ) 
+  )
+)
+
+(defn assign-account-type
+  "determines what the appropriate account type is for the user"
+  [employee-type-data]
+  (def employee-type-id (employee-type-data :worker_type_id))
+  (def account-type-map {
+    "STF" 3
+    "STU" 3
+    "FAC" 2
+    "LA" 1 ;; I've no clue what lab assistant will come up as, if it comes up as anything... It may not ever be assigned. BDR
+  })
+  (if (contains? account-type-map employee-type-id)
+    (get account-type-map employee-type-id) ;; get id since it is present in the map
+    4 ;; else
+  )
+)
+
+(defn get-employee-summary
+  "gets information about the employee which can be used for other queries"
+  [workerid byuid personid]
+  (def response (client/get (str "https://api.byu.edu/bdp/human_resources/worker_summary/v1/?worker_id=" workerid)
+                            {:headers {"Authorization" (ut/get-oauth-token-new)}}))
+  (def body (response :body))
+  (def json (json/read-str body))
+  (def walk-result (walk/keywordize-keys json)) ;; remember to include walk library
+  (def data_array (walk-result :data))
+  (def data (first data_array))
+  (def employee_type_data (get-employee-type (data :positions)))
+  {
+    :full-name (str (data :preferred_first_name) " " (data :preferred_last_name))
+    :byu-id byuid
+    :email (data :work_email_address)
+    :account-type (assign-account-type employee_type_data)
+    :person-id personid
+  }
+)
+
+(defn get-student-summary
+  "gets information about the student that can be used in other queries"
+  [netid personid]
+  (def response (client/get (str "https://api.byu.edu/bdp/student_academics/records/active_student_contact/v1/?net_id=" netid)
+                            {:headers {"Authorization" (ut/get-oauth-token-new)}}))
+  (def body (response :body))
+  (def json (json/read-str body))
+  (def walk-result (walk/keywordize-keys json)) ;; remember to include walk library
+  (def data_array (walk-result :data))
+  (def data (first data_array))
+  {
+    :full-name (data :preferred_name)
+    :byu-id (data :byu_id)
+    :email (data :student_email_address)
+    :account-type 3
+    :person-id personid
+  }
+)
+
+(defn get-user-data-new
+  "Gets data from AcademicRecordsStudentStatusInfo"
+  [byuid netid personid] ;; (def netid "nbown16") (def netid "rjr45")
+  (if (= (:front-end-netid env) netid)
+    {:full-name netid
+     :byu-id nil
+     :email (str netid "@yvideobeta.byu.edu")
+     :account-type 0
+     :person-id "000000000"}
+    ;; see if the user is an employee
+    ;; if so, get employee summary
+    ;; otherwise get student summary
+    (try
+      (def workerid (get-worker-id byuid))
+      (if (is-worker-id-empty workerid)
+        (get-student-summary netid personid)
+        (get-employee-summary workerid byuid personid)
+      )
+      (catch Exception e
+        {
+         :full-name netid
+         :byu-id nil
+         :email (str netid "@yvideobeta.byu.edu")
+         :account-type (if (:test env) 3 4)
+         :person-id "000000000"
+        }
+      )
+    )))
