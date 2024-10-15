5023989a4c224bdac2ef4d30bda2be3d88665d9a
Ben Rencher <rencherb@byu.edu>
2024-10-15 14:06:58 -0600

    digging deeper into what 'parameters' is and whether it is a valid key

--- src/clj/y_video_back/routes/home.clj
+++ src/clj/y_video_back/routes/home.clj
@@ -80,6 +80,7 @@
                                                               :byuid (get (get request :cas-info) :byuId)
                                                               :personid (get (get request :cas-info) :personId)
                                                               :sessionId (get (get request :parameters) :header)
+                                                              :parameters (get request :parameters)
                                                               ;; :byuid (get (get request :cas-info) :byuId) ;; get's byuid as expected. This pattern can be used to get any data in cas-info
                                                             }})}]
         ;;  ["/control-date-test" {:get (fn [request]
