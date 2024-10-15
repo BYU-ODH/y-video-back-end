a03b20ffe606f902594964221ffe9fa567148ae5
Ben Rencher <rencherb@byu.edu>
2024-10-15 11:49:47 -0600

    restructuring the code for student_schedule

--- src/clj/y_video_back/apis/student_schedule.clj
+++ src/clj/y_video_back/apis/student_schedule.clj
@@ -4,31 +4,36 @@
     [clj-http.client :as client]
     [clojure.data.json :as json]))
 
-;; new json parse for courses
-(defn get-courses-from-json-new
-  [json-obj]
-  (get-in json-obj ["data"]) ;; I believe the highest level attribute is "response" and I know that "data" is where the classes are stored in the response
-)
+;; new data extractor to use with new api call - BDR 9/5/2024
+;; (defn extract-course-data-new
+;;   [course]
+;;   {
+;;     :department (get-in course ["teaching_area"])
+;;     :catalog-number (get-in course ["catalog_number"])
+;;     :section-number (get-in course ["section_number"])
+;;   }
+;; )
 
-(defn get-courses-from-json
-  [js]
-  (get-in js ["WeeklySchedService" "response" "schedule_table"]))
+
+
+;; new api for getting student's courses - Ben Rencher 9/5/2024. may or may not use this since the other API isn't being retired
+;; (defn get-api-courses-new
+;;   [netid]
+;;   (def response (client/get (str "https://api.byu.edu/bdp/student_academics/records/current_student_class_enrollments/v1/?net_id=" netid "&year_term=" (ut/get-current-sem-real))
+;;                 {:headers {"Authorization" (ut/get-oauth-token-new)}}
+;;   ))
+;;   (def body (response :body))
+;;   (def json (json/read-str body))
+;;   (def walk-result (walk/keywordize-keys json))
+;;   (def data (walk-result :data))
+;;   (map extract-course-data-new data)
+;; )
 
 ;; we shouldn't need this anymore because the data returned by the bdp endpoint is arranged differently - BDR 9/5/2024
 (defn course-to-dep-and-cat
   "Returns vector. 0 is course, 1 is department, 2 is catalog number"
   [course]
   (re-find (re-matcher #"(.*) ([0-9]{3}\w{0,1})" course)))
-
-;; new data extractor to use with new api call - BDR 9/5/2024
-(defn extract-course-data-new
-  [course]
-  {
-    :department (get-in course ["teaching_area"])
-    :catalog-number (get-in course ["catalog_number"])
-    :section-number (get-in course ["section_number"])
-  }
-)
 
 (defn extract-course-data
   [course]
@@ -37,17 +42,9 @@
      :catalog-number (get dep-cat-vector 2)
      :section-number (get-in course ["section"])}))
 
-;; new api for getting student's courses - Ben Rencher 9/5/2024
-(defn get-api-courses-new
-  [netid]
-  (let [url (str "https://api.byu.edu/bdp/student_academics/records/current_student_class_enrollments/v1/?net_id=" netid "&year_term=" (ut/get-current-sem-real-new))
-        res (try (client/get url {:oath-token ut/oauth-token})
-                 (catch Exception e
-                    (client/get url {:oath-token (ut/get-oauth-token-new)})))
-        json-res (json/read-str (:body res))]
-    (map extract-course-data-new (get-courses-from-json-new json-res))
-  )
-)
+(defn get-courses-from-json
+  [js]
+  (get-in js ["WeeklySchedService" "response" "schedule_table"]))
 
 (defn get-api-courses
   [person-id]
@@ -55,7 +52,7 @@
     []
     (try
     ;; this api isn't going away, and we need to keep it for the time being. BDR 10/14/2024
-      (let [url (str "https://api.byu.edu:443/domains/legacy/academic/registration/studentschedule/v1/" person-id "/" (ut/get-current-sem))
+      (let [url (str "https://api.byu.edu:443/domains/legacy/academic/registration/studentschedule/v1/" person-id "/" (ut/get-current-sem-real))
             res (try (client/get url {:oauth-token ut/oauth-token})
                      (catch Exception e
                        (client/get url {:oauth-token (ut/get-oauth-token-new)})))
